19ed7a610bb0ff0c413afd36546d5496c4aed578
Davorin Učakar <davorin.ucakar@gmail.com>
2016-04-04 18:13:17 +0200

    Fixed for KSP 1.1

--- TextureReplacer/Replacer.cs
+++ TextureReplacer/Replacer.cs
@@ -23,6 +23,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using KSP.UI.Screens.Flight;
 using UnityEngine;
 
 namespace TextureReplacer
@@ -102,14 +103,14 @@ namespace TextureReplacer
       if (hudNavBallTexture != null) {
         NavBall hudNavball = UnityEngine.Object.FindObjectOfType<NavBall>();
         if (hudNavball != null) {
-          hudNavball.navBall.renderer.sharedMaterial.mainTexture = hudNavBallTexture;
+          hudNavball.navBall.GetComponent<Renderer>().sharedMaterial.mainTexture = hudNavBallTexture;
         }
       }
 
       if (ivaNavBallTexture != null && InternalSpace.Instance != null) {
         InternalNavBall ivaNavball = InternalSpace.Instance.GetComponentInChildren<InternalNavBall>();
         if (ivaNavball != null) {
-          ivaNavball.navBall.renderer.sharedMaterial.mainTexture = ivaNavBallTexture;
+          ivaNavball.navBall.GetComponent<Renderer>().sharedMaterial.mainTexture = ivaNavBallTexture;
         }
       }
     }
